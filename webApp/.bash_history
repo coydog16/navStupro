accf6ffceb1df666f4e82b4cd372f73396203694
coydog16 <coydog16@gmail.com>
2025-05-10 19:58:26 +0900

    breezeInstall@React+Inartia+TypeScript

---
+++ webApp/.bash_history
@@ -0,0 +1,8 @@
+php artisan laravel/breeze --dev
+composer require laravel/breeze --dev
+php artisan
+php artisan breeze install
+yes
+php artisan breeze:install
+npm run dev
+exit
--- webApp/vite.config.js
+++ webApp/vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.tsx',
+            ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
-        tailwindcss(),
+        react(),
     ],
 });
